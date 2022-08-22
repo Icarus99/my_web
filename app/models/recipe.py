0e98656519307a795eb0290ec6c8b9e84887a933
Icarus99 <jiajun_li@ucsb.edu>
2022-08-22 15:01:17 +0800

    modified db

--- app/models/recipe.py
+++ app/models/recipe.py
@@ -1,10 +1,9 @@
 from sqlalchemy import Column, Integer, String
 from flask_sqlalchemy import SQLAlchemy
+from app.models.base import db, Base
 
-db = SQLAlchemy()
 
-
-class recipe(db.Model):
+class recipe(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     summary = Column(String(1000))
     image = Column(String(50))
